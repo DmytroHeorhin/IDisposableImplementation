2d18008626564c83fc0807ca5cc39095eecec981
Heorhin, Dmytro <Dmytro.Heorhin@wolterskluwer.com>
2017-05-13 19:24:08 +0300

    Fix resources releasing

---
+++ IDisposableImplementation/MemoryStreamLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace IDisposableImplementation
+{
+    public class MemoryStreamLogger : IDisposable
+    {
+        private readonly StreamWriter _streamWriter;
+        private bool _isDisposed;
+
+        public MemoryStreamLogger()
+        {
+            _streamWriter = new StreamWriter(@"\log.txt", append: true);
+        }
+
+        public void Log(string message)
+        {
+            if(_isDisposed)
+                throw new ObjectDisposedException(nameof(MemoryStreamLogger));
+
+            _streamWriter.WriteLine(message);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_isDisposed) return;
+
+            if (disposing)
+            {
+                _streamWriter.Close();
+            }
+
+            _isDisposed = true;
+        }
+    }
+}
--- IDisposableImplementation/Program.cs
+++ IDisposableImplementation/Program.cs
@@ -8,8 +8,22 @@ namespace IDisposableImplementation
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            for (var i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Writing #" + i + "...");
+                WriteLog("Iteration number #" + i);
+            }
+
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+        }
+
+        private static void WriteLog(string str)
+        {
+            using (var logger = new MemoryStreamLogger())
+                logger.Log(str);
         }
     }
 }
